f0d0583058bc77ad8137f0d7ed3088488896f204
vsegda_vinovat <riabko97@gmail.com>
2025-04-07 22:32:07 +0300

    added footer

--- client/src/app/(nondashboard)/landing/call-to-action-section.tsx
+++ client/src/app/(nondashboard)/landing/call-to-action-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 export const CallToActionSection = () => {
   return (
@@ -11,39 +12,44 @@ export const CallToActionSection = () => {
         fill
         className="object-cover object-center"
       />
-      <div className="absolute inset-0 bg-black bg-opacity-60">
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          transition={{
-            duration: 0.5,
-          }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          className="relative max-w-4xl xl:max-w-6xl mx-auto px-6 sm:px-8 lg:px-12 xl:px-16 py-12"
-        >
-          <div className="flex flex-col md:flex-row justify-between items-center">
-            <div className="mb-6 md:mb-0 md:mr-10">
-              <h2 className="text-2xl font-bold text-white">
-                Find Your Dream Rental Property
-              </h2>
-            </div>
-            <div>
-              <p className="text-white mb-3">
-                Discover a wide range of propertiesin in your desired location
-              </p>
-              <div className="flex justify-center md:justify-start gap-4">
-                <button
-                  onClick={() =>
-                    window.scrollTo({ top: 0, behavior: "smooth" })
-                  }
-                >
-                  Search
-                </button>
-              </div>
+      <div className="absolute inset-0 bg-black bg-opacity-60"> </div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        transition={{
+          duration: 0.5,
+        }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="relative max-w-4xl xl:max-w-6xl mx-auto px-6 sm:px-8 lg:px-12 xl:px-16 py-12"
+      >
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-6 md:mb-0 md:mr-10">
+            <h2 className="text-2xl font-bold text-white">
+              Find Your Dream Rental Property
+            </h2>
+          </div>
+          <div>
+            <p className="text-white mb-3">
+              Discover a wide range of propertiesin in your desired location
+            </p>
+            <div className="flex justify-center md:justify-start gap-4">
+              <button
+                className="inline-block text-primary-700 bg-white rounded-lg px-6 py-3 font-semibold hover:bg-primary-50"
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              >
+                Search
+              </button>
+              <Link
+                className="inline-block text-white bg-secondary-500 rounded-lg px-6 py-3 font-semibold hover:bg-secondary-600"
+                href={"/signup"}
+                scroll={false}
+              >
+                Sign Up
+              </Link>
             </div>
           </div>
-        </motion.div>
-      </div>
+        </div>
+      </motion.div>
     </div>
   );
 };
--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,6 +1,7 @@
 import { CallToActionSection } from "./call-to-action-section";
 import { DiscoverSection } from "./discover-section";
 import { FeaturesSection } from "./features-section";
+import { FooterSection } from "./footer-section";
 import { HeroSection } from "./hero-section";
 
 export default function LandingPage() {
@@ -10,6 +11,7 @@ export default function LandingPage() {
       <FeaturesSection />
       <DiscoverSection />
       <CallToActionSection />
+      <FooterSection />
     </div>
   );
 }
