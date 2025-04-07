9937b05fa51eed0463c63eb489965e5589c9293e
vsegda_vinovat <riabko97@gmail.com>
2025-04-07 13:01:58 +0300

    create call to action

---
+++ client/src/app/(nondashboard)/landing/call-to-action.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const CallToAction = () => {
+  return (
+    <div className="relative py-24">
+      <Image src={"/landing-call-to-action"} alt="landing-call-to-action" />
+    </div>
+  );
+};
