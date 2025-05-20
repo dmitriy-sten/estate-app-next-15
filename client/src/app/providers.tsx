7282ee325f2d0384c683f043dafc902dec325376
vsegda_vinovat <riabko97@gmail.com>
2025-05-20 13:57:28 +0300

    new

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,12 +1,20 @@
-'use client'
+"use client";
 
 import StoreProvider from "@/state/redux";
+import { Authenticator } from "@aws-amplify/ui-react";
 import React, { ReactNode } from "react";
+import { Auth } from "./(auth)/authProvider";
 
 interface Props {
   children: ReactNode;
 }
 
 export const Providers: React.FC<Props> = ({ children }) => {
-  return <StoreProvider>{children}</StoreProvider>;
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  );
 };
