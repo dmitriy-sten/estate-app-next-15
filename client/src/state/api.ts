87b11468515063447c7080f41a01ddad6ae6ba1b
vsegda_vinovat <riabko97@gmail.com>
2025-05-20 13:57:23 +0300

    added api instance

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
@@ -6,7 +7,30 @@ export const api = createApi({
   }),
   reducerPath: "api",
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+
+    getAuthUser: build.query<User>({
+      queryFn: async (_, _queryApi, _extraOptions, fetchWithBQ) => {
+        try {
+
+          const session = await fetchAuthSession()
+          const { idToken } = session.tokens ?? {}
+          const user = await getCurrentUser()
+          const userRole = idToken?.payload['custom:role'] as string
+
+
+          const endpoint =
+            userRole === 'manager' ?
+              `/manager/${user.userId}` :
+              `/tenants/${user.userId}`
+        }
+        catch (error) {
+
+        }
+      }
+    })
+
+  }),
 });
 
-export const {} = api;
+export const { } = api;
