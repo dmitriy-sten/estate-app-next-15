074882c7747bba39ffca95bae1ff2952060b8edc
vsegda_vinovat <riabko97@gmail.com>
2025-05-23 15:30:55 +0300

    new

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,15 +1,26 @@
+import { Manager, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    prepareHeaders: async (headers) => {
+      const session = await fetchAuthSession()
+      const { idToken } = session.tokens ?? {}
+
+      if (idToken) {
+        headers.set('Authorization', `Bearer ${idToken}`)
+      }
+
+      return headers
+    }
   }),
   reducerPath: "api",
   tagTypes: [],
   endpoints: (build) => ({
 
-    getAuthUser: build.query<User>({
+    getAuthUser: build.query<User, void>({
       queryFn: async (_, _queryApi, _extraOptions, fetchWithBQ) => {
         try {
 
@@ -23,8 +34,24 @@ export const api = createApi({
             userRole === 'manager' ?
               `/manager/${user.userId}` :
               `/tenants/${user.userId}`
+
+
+          let userDetailResponse = await fetchWithBQ(endpoint)
+
+
+          return {
+            data: {
+              cognitoInfo: { ...user },
+              userInfo: userDetailResponse.data as Tenant | Manager,
+              userRole
+            }
+          }
         }
-        catch (error) {
+        catch (error: any) {
+
+          return {
+            error: error.message || "Could not fetch user data"
+          }
 
         }
       }
@@ -33,4 +60,4 @@ export const api = createApi({
   }),
 });
 
-export const { } = api;
+// export const { } = api;
